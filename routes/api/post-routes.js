5b49c35abb3bfcd339b6bfcbc4f6ffe11ce71398
Ridercogswell <ridercogswell@gmail.com>
2022-03-24 18:59:59 -0700

    push to heroku

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,25 +1,34 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
 const sequelize = require('../../config/connection');
-const { up } = require('inquirer/lib/utils/readline');
-
+const { Post, User, Vote, Comment } = require('../../models');
 // GET all users
 router.get('/', (req, res) => {
     Post.findAll({
-        attributes: [
-            'id', 
-            'post_url', 
-            'title', 
-            'created_at',
-            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      order: [['created_at', 'DESC']],
+      attributes: [
+        'id', 
+        'post_url', 
+        'title', 
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-        order: [['created_at', 'DESC']],
-        include: [
-            {
+    include: 
+    [
+        // comment model
+        {
+            model: Comment, 
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                // *******comment model includes the User model itself so it can attach the username to the comment
                 model: User,
                 attributes: ['username']
             }
-        ]
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
+    ]
     })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
@@ -39,13 +48,21 @@ router.get('/:id', (req, res) => {
             'post_url', 
             'title', 
             'created_at',
-            [sequelize.literal('SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-    ],
-        include: [
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [ 
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
-            }
+            },
         ]
     })
     .then(dbPostData => {
@@ -161,7 +178,5 @@ router.delete('/:id', (req, res) => {
         res.status(500).json(err);
     });
 });
-
-
 
 module.exports = router;
